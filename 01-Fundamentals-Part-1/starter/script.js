25456df823d2846c2fec436a4e2d238a625fd7cf
Phil Burkhill <philip.burkhill@gmail.com>
2022-03-05 02:03:31 +0000

    code challenge 3 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -36,3 +36,40 @@ if (population > averagePop){
     console.log(`${country}'s population is ${averagePop - population} million below average.`)
 }
 
+//Seventh task - type conversion and coercion
+let n = '1' + 1;//11
+n = n - 1;//10
+console.log(n);//Will output n
+console.log(9 + '5')//Javascript (+ operator will convert numbers to strings).
+console.log('9'-'5');//Javascript (- operator converts to numbers). 
+console.log('19' - '13' + '17'); // "617"
+console.log('19' - '13' + 17); // 23
+console.log("123" < 57); //False
+console.log(5 + 6 + '4' + 9 - 4 - 2);//*11)4(3)
+
+//Eigth Task - Truthy and Falsy
+
+// 5 falsy values: 0, '', undefined, null, NaN
+// Anything else is truthy.
+
+//Ninth Task - Inequality operators == vs ===
+//=== doesn't perform type coercion, only returns true when both are EXACTLY the same.
+//== loose equality does type coercion, so strings can equal numbers. 
+//!== (strictly), !=== (strictly not) also works.
+
+//let numNeighbours = Number(prompt("How many countries border your own country?","No. of borders?"));
+//This uses a string by default. Number forces it to use number. Commented out to get rid of prompt.
+// if (numNeighbours === 1) {
+//     console.log("Only 1 border!")
+// } else if (numNeighbours > 1) {
+//     console.log("More than 1 border!") 
+// } else {console.log("No borders.")
+// }
+
+//Tenth Task - Logical Operators
+
+if (language === "English" && population < 50 && isIsland){
+    console.log(`You should live in ${country}`)
+}   else {console.log(`${country} does not meet your criteria.`)
+
+}
